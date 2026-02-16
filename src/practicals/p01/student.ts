d2f2c9c87aed481c0d689af24c53ddd625ee59b1
68010876-Phoomrapee <68010876@kmitl.ac.th>
2026-02-16 09:58:34 +0700

    p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,13 @@
-export class Student {}
+export class Student {
+    firstname: string
+    lastname: string
+
+    constructor(firstname: string, lastname: string) {
+        this.firstname = firstname
+        this.lastname = lastname
+    }
+}
+
+const student = new Student('John', 'Doe')
+
+console.log(student.firstname + ' ' + student.lastname)
